d745a6549ce5be21654135c096986bf35c9943e4
Mauri García <63955780+maurigarciavg@users.noreply.github.com>
2026-03-09 20:39:33 +0000

    first changes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,8 +5,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Planet, Character
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -26,26 +23,33 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_users():
+    users_query = User.query.all()
+    results = list(map(lambda item: item.serialize(), users_query))
+    return jsonify(results), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people', methods=['GET'])
+def get_character():
+    character_query = Character.query.all()
+    results = list(map(lambda item: item.serialize(), character_query))
+    return jsonify(results), 200 
 
-    return jsonify(response_body), 200
+@app.route('/people/<int:people.id>', methods=['GET'])
+def get_character_id():
+    character_id_query = Character.query.all()
+    results = list(map(lambda item: item.serialize(), character_id_query))
+    return jsonify(results), 200 
 
-# this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
